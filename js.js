34707aaccae9b5eaebf5682042c5d8703db3c6d0
Akaki <akakikoridze@yahoo.com>
2019-11-06 18:57:50 +0400

    bug fixed

--- js.js
+++ js.js
@@ -89,10 +89,11 @@ function bestSpot() {
 }
 
 function checkTie() {
-    if (emptySquares().length == 0){
+    if (emptySquares().length == 0 && gameWon.player != humPlayer){
         for (var i = 0; i < cells.length; i++) {
             cells[i].style.backgroundColor = 'green';
             cells[i].removeEventListener('click', turnClick, false)
+   
         }
         declareWinner('Tie Game!')
         return true;
